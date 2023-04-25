cfd5bb91d40396077c28786873c179515d766f51
Daywid <88506180+daywid@users.noreply.github.com>
2023-04-24 21:51:48 -0300

    Add files via upload

--- aula_maven/src/main/java/mapeamento/Consumidor_Estante.java
+++ aula_maven/src/main/java/mapeamento/Consumidor_Estante.java
@@ -7,7 +7,7 @@ public class Consumidor_Estante {
 		Prateleira prateleira = new Prateleira();
 		Estante estante = new Estante();
 		
-		//Criar 3 livros e adicionar a prateleira
+		//Criar 2 livros e adicionar a prateleira
 		livro.autor = "Graciliano Ramos";
 		livro.titulo = "Vidas Secas";
 		livro.edicao = "2a. Edição";
@@ -22,10 +22,7 @@ public class Consumidor_Estante {
 		
 		estante.prateleiras.add(prateleira);
 		
-		
-		
 		System.out.println("Qtd de livros:" + prateleira.Livros.size());
-		
 		System.out.println(prateleira.listar_titulos());
 		
 		/*System.out.println(prateleira.Livros.size());
@@ -36,6 +33,19 @@ public class Consumidor_Estante {
 		}
 		
 	}*/
-
+		
+		System.out.println("Numero de prateleiras na estante:" + estante.prateleiras.size());
+		System.out.println("Numero de livros na 1a. prateleira:" + prateleira.Livros.size());
+		
+		//navegar em todas as prateleiras da estante e imprimir os titulos de todos os livros
+		for (int j=0;j<=estante.prateleiras.size();j++) {
+			
+			System.out.println("Prateleira "+j + ":");
+			//navegar na prateleira
+			for (int i=0;i<estante.prateleiras.get(j).Livros.size();i++) {
+				
+			}
+			
+		}
 }
 	}
