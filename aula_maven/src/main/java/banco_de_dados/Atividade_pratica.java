60f376b4552fbeee3dd8b6ec3f9c3e9ac40e8a28
Daywid <88506180+daywid@users.noreply.github.com>
2023-05-08 21:39:14 -0300

    Add files via upload

---
+++ aula_maven/src/main/java/banco_de_dados/Atividade_pratica.java
@@ -0,0 +1,20 @@
+package banco_de_dados;
+
+public class Atividade_pratica {
+
+	/*A empresa Coisas & Coisas solicitou o desenvolvimento de um Sistema de gerenciamento de atendimentos aos seus clientes. Para tanto, faça o que se pede:
+
+a. Implemente uma classe que represente os clientes da empresa;
+b. Implemente uma classe que represente os serviços prestados pela empresa (Reparos elétricos, Reparos hidráulicos e Reparos em alvenaria)
+c. Implemente uma classe que represente os prestadores de serviços da empresa
+
+d. Implemente uma classe que represente os serviços contratados pelos clientes. Deve-se gravar a data do atendimento, os dados do cliente, os dados dos serviços prestados e respective valores.
+
+Atenção:1. Inicie pelo banco de dados
+2. Cada classe deve possuir os métodos find_one(); salve() e delete()
+3. O save() deve ser inteligente para acionar tanto o insert quanto o update do banco de dados
+4. Respeite as formas normais, incluindo a Boyce Codd
+*/
+	
+	
+}
